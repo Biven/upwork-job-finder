c4e7a9052234db806f7c7853b3d5b5326e027c35
Yauheni Z <yauhenizubovich@cloud.upwork.com>
2019-10-10 07:58:17 +0300

    Single commit

---
+++ src/main/java/com/upwork/gittest/TestClass.java
@@ -0,0 +1,11 @@
+package com.upwork.gittest;
+
+/**
+ * @author Yauheni Zubovich
+ */
+public class TestClass {
+
+    public static void main(String[] args) {
+        System.out.println("Commit 2");
+    }
+}
